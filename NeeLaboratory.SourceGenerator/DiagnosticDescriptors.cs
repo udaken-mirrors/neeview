5c65194a0a9a6e586f3bf491c8ee66057fdab28c
Ito Mitsuhiro <nee.laboratory@gmail.com>
2023-08-10 14:05:50 +0900

    - SourceGenerator 導入

---
+++ NeeLaboratory.SourceGenerator/DiagnosticDescriptors.cs
@@ -0,0 +1,18 @@
+﻿#pragma warning disable RS2008 // アナライザー リリース追跡を有効にする
+using Microsoft.CodeAnalysis;
+
+namespace NeeLaboratory.SourceGenerator;
+
+public static class DiagnosticDescriptors
+{
+    const string Category = "NeeLaboratory.SourceGenerator";
+
+    // INotifyPropertyChanged が必要
+    public static readonly DiagnosticDescriptor INotifyPropertyChangedRequired = new(
+        id: "NSG1001",
+        title: "INotifyPropertyChanged required",
+        messageFormat: "class '{0}' requires INotifyPropertyChanged",
+        category: Category,
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+}
---
+++ NeeLaboratory.SourceGenerator/Tools/PathTools.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Linq;
+
+namespace NeeLaboratory.SourceGenerator.Tools;
+
+public static class PathTools
+{
+    public static string ReplaceInvalidFileNameChars(string s)
+    {
+        if (s is null) return "";
+
+        var invalidChars = System.IO.Path.GetInvalidFileNameChars();
+        return string.Concat(s.Select(c => invalidChars.Contains(c) ? '_' : c));
+    }
+}
